509896ee485c2716f21a61c69f1d6f139230da8e
Алексей <107575283+alexey4050@users.noreply.github.com>
2023-09-29 10:55:34 +0300

    Initial Commit

---
+++ HomeworkApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkApp3
+//
+//  Created by testing on 29.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
